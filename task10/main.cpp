35329ebbc1fcef25562bb684231b575f8fbf05e7
Zaiqiang Wu <wuzaiqiang@zju.edu.cn>
2023-07-03 22:11:19 +0900

    task10 finished

--- task10/main.cpp
+++ task10/main.cpp
@@ -107,8 +107,8 @@ int main() {
         time += dt;
         // Write some code below to simulate rotation of the rigid body
         // Use the **forward Euler method** to update the rotation matrix and the angular velocity
-        // rotation =
-        // Omega =
+        rotation = rotation*Eigen::AngleAxisf(Omega.norm()*dt,Omega.stableNormalized());
+        Omega = Omega - inertia.inverse()*Omega.cross(inertia*Omega)*dt;
         // Do not change anything else except for the two lines above.
       }
       std::cout << "time: " << time << std::endl;
